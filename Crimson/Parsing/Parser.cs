c1c4c0ec073f5f662dbd375b18dc55f0e80793cd
Abhinav Jha <linuxlenin04@gmail.com>
2025-01-03 13:57:50 +0530

    removed partial classes

--- Crimson/Parsing/Parser.cs
+++ Crimson/Parsing/Parser.cs
@@ -83,6 +83,131 @@ internal partial class Parser
             Program.Add(stmt);
         }
     }
+
+    internal Stmt ParseStmt()
+    {
+        switch (_currToken.Kind)
+        {
+            case TokenKind.Print:
+                return ParsePrintStmt();
+            case TokenKind.PrintAst:
+                return ParsePrintAstStmt();
+            default:
+                return ParseExprStmt((int)BindingPower.None);
+        }
+    }
+
+    internal Stmt ParseExprStmt(int bindingPower)
+    {
+        Expr expr = ParseExpr(bindingPower);
+        Advance();
+        Expect(TokenKind.Semicolon, "");
+        return new ExprStmt(expr);
+    }
+
+    internal Stmt ParsePrintStmt()
+    {
+        Advance();
+        Expr expr = ParseExpr((int)BindingPower.None);
+        Advance();
+        Expect(TokenKind.Semicolon, "Print statement must end with a semicolon");
+        return new PrintStmt(expr);
+    }
+
+    internal Stmt ParsePrintAstStmt()
+    {
+        Advance();
+        Expr expr = ParseExpr((int)BindingPower.None);
+        Advance();
+        Expect(TokenKind.Semicolon, "PrintAst statement must end with a semicolon");
+        return new PrintAstStmt(expr);
+    }
+
+    // Method for Pratt Parsing Expressions
+    internal Expr ParseExpr(int bindingPower)
+    {
+        if (_prefixes.TryGetValue(_currToken.Kind, out PrefixParselet prefixParselet))
+        {
+            Expr left = prefixParselet((int)BindingPower.Prefix);
+
+            while (Peek().Kind != TokenKind.Eof && Peek().Kind != TokenKind.Semicolon && bindingPower < PeekBindingPower())
+            {
+                Advance();
+                if (_infixes.TryGetValue(_currToken.Kind, out InfixParselet infixParselet))
+                    left = infixParselet(left, PeekBindingPower());
+            }
+
+            return left;
+        }
+
+        return null;
+    }
+
+    private int PeekBindingPower()
+    {
+        if (_infixBindingPower.TryGetValue(Peek().Kind, out int bp))
+            return bp;
+        return (int)BindingPower.None;
+    }
+
+    internal Expr ParseNumLitExpr(int bindingPower)
+    {
+        string literal = _currToken.Literal;
+        
+        if (double.TryParse(literal, out double doubleLiteral))
+        {
+            return new NumLitExpr(doubleLiteral, _currToken);
+        }
+
+        Console.WriteLine("Could not parse {0} as number (double) literal", literal);
+        Environment.Exit(0);
+        return null;
+    }
+
+    internal Expr ParseBoolLitExpr(int bindingPower)
+    {
+        string literal = _currToken.Literal;
+
+        if (literal == "true")
+        {
+            return new BoolLitExpr(true, _currToken);
+        }
+        else
+        {
+            return new BoolLitExpr(false, _currToken);
+        }
+
+    }
+
+    internal Expr ParseNilLitExpr(int bindingPower)
+    {
+        return new NilLitExpr();
+    }
+
+    internal Expr ParsePrefixExpr(int bindingPower)
+    {
+        TokenKind op = _currToken.Kind;
+        Advance();
+        Expr expr = ParseExpr(bindingPower);
+        return new PrefixExpr(op, expr);
+    }
+
+    internal Expr ParseGroupingExpr(int bindingPower)
+    {
+        Advance();
+        Expr expr = ParseExpr((int)BindingPower.None);
+        Advance();
+        Match(TokenKind.RParen, "");
+        return new GroupingExpr(expr);
+    }
+
+    internal Expr ParseBinaryExpr(Expr left, int bindingPower)
+    {
+        TokenKind op = _currToken.Kind;
+        Advance();
+        Expr right = ParseExpr(bindingPower);
+        return new BinaryExpr(op, left, right);
+    }
     
     private bool IsNotEof()
     {
